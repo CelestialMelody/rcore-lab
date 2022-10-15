7176a2d67b09220f2e282eac5d8c0103337a035b
clstilmldy <celestialmelodygo@gmail.com>
2022-10-15 20:03:00 +0800

    identical to os1

---
+++ os2/src/lang_items.rs
@@ -0,0 +1,19 @@
+use core::panic::PanicInfo;
+use crate::sbi::shutdown;
+// use crate::println; // main -> micro_use
+
+#[panic_handler] // 当遇到不可恢复错误的时候，被标记为语义项 #[panic_handler] 的 panic 函数将会被调用
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!(
+            "Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message().unwrap()
+        );
+    } else {
+        // 在 main.rs 开头加上 #![feature(panic_info_message)] 才能通过 PanicInfo::message 获取报错信息
+        println!("Panicked: {}", info.message().unwrap()); // main -> panic_info_message
+    }
+    shutdown()
+}
