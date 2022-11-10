e813fc7a283fa913757c9241be85f017252c68fc
clstilmldy <celestialmelodygo@gmail.com>
2022-11-10 17:12:30 +0800

    update

--- user/build.py
+++ user/build.py
@@ -1,44 +1,23 @@
 import os
 
 base_address = 0x80400000
-step = 0x20000 # 128k
+step = 0x20000
 linker = "src/linker.ld"
 
 app_id = 0
-apps = os.listdir("src/bin") # listdir: get files and filfloders
+apps = os.listdir("build/app")
 apps.sort()
 
+# we use `Clink-args=-Ttext=%x` to set the base address of the app
+# so do not need to change the linker.ld
 for app in apps:
-    app = app[: app.find(".")] # get the name of the app
-    lines = []
-    lines_before = []
-    with open(linker, "r") as f:
-        for line in f.readlines():
-            lines_before.append(line)
-            # replace the address of the app
-            line = line.replace(hex(base_address), hex(base_address + step * app_id))
-            lines.append(line)
-
-    #  write the new linker file
-    with open(linker, "w+") as f:
-        f.writelines(lines)
-
-    # os.system(
-    #     "cargo rustc --bin %s --release -- -Clink-arg=-Ttext=%x" # 使用 --bin 参数来只构建某一个应用
-    #     % (app, base_address + step * app_id)
-    # )
-
+    app = app[: app.find(".")]
     os.system(
-        "cargo rustc --bin %s --release" # 使用 --bin 参数来只构建某一个应用
-        % app
+        "cargo rustc --bin %s --release -- -Clink-args=-Ttext=%x"
+        % (app, base_address + step * app_id)
     )
-
     print(
-        "[build.py] application %s strat with address %s"
+        "[build.py] application %s start with address %s"
         % (app, hex(base_address + step * app_id))
     )
-
-    # restore linker.ld
-    with open(linker, "w+") as f:
-        f.writelines(lines_before)
     app_id = app_id + 1
