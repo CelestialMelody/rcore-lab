78bb1545540c935404fe0c3edfff121a729936d3
clstilmldy <celestialmelodygo@gmail.com>
2022-11-12 13:30:14 +0800

    update:annotation

--- os3/src/config.rs
+++ os3/src/config.rs
@@ -7,5 +7,8 @@ pub const APP_SIZE_LIMIT: usize = 0x20000;
 pub const MAX_SYSCALL_NUM: usize = 500;
 
 /// 常数 CLOCK_FREQ 是一个预先获取到的各平台不同的时钟频率，单位为赫兹，也就是一秒钟之内计数器的增量;
-/// in qemu virt machine:
-pub const CLOCK_FREQ: usize = 12500000; // 12.5MHz
+/// 说是 CPU 主频，但实际就是 time 寄存器自增的频率而已，这个必须是一个稳定的值，真正 CPU 运行的频率不一定。
+pub const CLOCK_FREQ: usize = 0x989680; // 10MHz; see dump.dts -> 
+
+// in (old) qemu virt machine:
+// pub const CLOCK_FREQ: usize = 12500000; // 12.5MHz (old qemu)
