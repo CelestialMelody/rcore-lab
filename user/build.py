a77df999794593b7256712e9e51747af55febb07
clstilmldy <celestialmelodygo@gmail.com>
2022-11-12 14:26:35 +0800

    fix: for os2

--- user/build.py
+++ user/build.py
@@ -7,6 +7,7 @@ linker = "src/linker.ld"
 app_id = 0
 apps = os.listdir("build/app")
 apps.sort()
+chapter = os.getenv("CHAPTER")
 
 # we use `Clink-args=-Ttext=%x` to set the base address of the app
 # so do not need to change the linker.ld
@@ -20,4 +21,5 @@ for app in apps:
         "[build.py] application %s start with address %s"
         % (app, hex(base_address + step * app_id))
     )
-    app_id = app_id + 1
+    if chapter == "3" or int(chapter) == 3:
+        app_id = app_id + 1
