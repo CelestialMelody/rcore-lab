fb60d948e1ec3befcfbb1f375c4fd75425f62403
melody <CelestialMelodyGo@gmail.com>
2023-01-05 17:27:54 +0800

    update

--- os4/build.rs
+++ os4/build.rs
@@ -52,6 +52,12 @@ _num_apps:
         apps.len() - 1
     )?;
 
+    // 应用构建器 os/build.rs 的改动：
+    // - 首先，我们在 .incbin 中不再插入清除全部符号的应用二进制镜像 *.bin ，而是将应用的 ELF 执行文件直接链接进来；
+    // - 其次，在链接每个 ELF 执行文件之前我们都加入一行 .align 3 来确保它们对齐到 8 字节，这是由于如果不这样做，
+    //  xmas-elf crate 可能会在解析 ELF 的时候进行不对齐的内存读写，
+    //  例如使用 ld 指令从内存的一个没有对齐到 8 字节的地址加载一个 64 位的值到一个通用寄存器。
+    //  而在 k210 平台上，由于其硬件限制，这种情况会触发一个内存读写不对齐的异常，导致解析无法正常完成。
     for (idx, app) in apps.iter().enumerate() {
         println!("app_{}: {}", idx, app);
         writeln!(
@@ -60,8 +66,9 @@ _num_apps:
     .section .data
     .global _app_{0}_start
     .global _app_{0}_end
+    .align 3
 _app_{0}_start:
-    .incbin "{2}{1}.bin"
+    .incbin "{2}{1}.elf"
 _app_{0}_end:"#,
             idx, app, TARGET_PATH
         )?;
